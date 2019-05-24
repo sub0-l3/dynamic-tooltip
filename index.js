711f3cd83d46ac31c2f606228e4bda2765f5d7f1
sub0-l3 <subhrajit.debnath@gmail.com>
2019-05-24 15:03:09 +0530

    Fix click not working by shifting logic to click evt listener

--- index.js
+++ index.js
@@ -31,69 +31,66 @@ function wrapWords(str) {
   return str.replace(/(\S+\s*)/g, "<span>$&</span>");
 }
 
-function insertBreakAtPoint(e) {
-  e.preventDefault();
-  let range;
-  let textNode;
-  let offset;
-  console.log("X: ", e.pageX, "Y: ", e.pageY);
+function insertBreakAtPoint(e) {}
 
-  if (document.caretPositionFromPoint) {
-    // standard
-    range = document.caretPositionFromPoint(e.pageX, e.pageY);
-    textNode = range.offsetNode;
-    offset = range.offset;
-  } else if (document.caretRangeFromPoint) {
-    // WebKit
-    range = document.caretRangeFromPoint(e.pageX, e.pageY);
-    textNode = range.startContainer;
-    offset = range.startOffset;
-  }
+document.addEventListener(
+  "click",
+  function(e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement,
+      textNode = target.firstChild;
+    e.preventDefault();
+    if (
+      !(target.nodeType === 1 && target.nodeName === "SPAN") ||
+      target.classList.contains("tooltip") ||
+      !(textNode.nodeType === 3)
+    )
+      return;
 
-  // do whatever you want here!
-  // console.log(range);
-  // console.log(textNode);
-  // console.log(offset);
-  textNode.parentElement.classList.add("tooltip");
+    textNode.parentElement.classList.add("tooltip");
 
-  let para = document.createElement("span");
-  let node = document.createTextNode(
-    `--------------${
-      textNode.textContent
-    }-------------Tony Montana and his close friend Manny, build a strong drug empire in Miami. However as his power begins to grow, so does his ego and his enemies, and his own paranoia begins to plague his empire...`
-  );
-  para.appendChild(node);
-  para.className = "tooltiptext";
-  textNode.parentElement.appendChild(para);
+    let para = document.createElement("span");
+    let node = document.createTextNode(
+      `--------------${
+        textNode.textContent
+      }-------------Tony Montana and his close friend Manny, build a strong drug empire in Miami. However as his power begins to grow, so does his ego and his enemies, and his own paranoia begins to plague his empire...`
+    );
+    para.appendChild(node);
+    para.className = "tooltiptext";
+    textNode.parentElement.appendChild(para);
 
-  // coordinates of span element
-  let coordXYContainer = document
-    .getElementById("storyReader")
-    .getBoundingClientRect();
-  let coordXY = textNode.parentElement.getBoundingClientRect();
-  document.getElementById("coordinates").innerHTML = `Top: ${
-    coordXY.top
-  }, Left: ${coordXY.left}, Bottom: ${coordXY.bottom}, Right: ${coordXY.right}`; // coordinates is id in HTML doc
-  document.getElementById(
-    "coordinates-container"
-  ).innerHTML = `Top: ${coordXY.top -
-    coordXYContainer.top}, Left: ${coordXY.left -
-    coordXYContainer.left}, Bottom: ${coordXYContainer.bottom -
-    coordXY.bottom}, Right: ${coordXYContainer.right - coordXY.right}`;
-  let spaceObj = {
-    top: coordXY.top - coordXYContainer.top,
-    left: coordXY.left - coordXYContainer.left,
-    bottom: coordXYContainer.bottom - coordXY.bottom,
-    right: coordXYContainer.right - coordXY.right
-  };
-  const maxSpace = Math.max(...Object.values(spaceObj));
-  let maxProp = Object.entries(spaceObj).filter(e => e[1] === maxSpace)[0];
-  // console.log(maxSpace)
-  //TODO: width and height of textNode (coordXY) to be taken into consideration
+    // coordinates of span element
+    let coordXYContainer = document
+      .getElementById("storyReader")
+      .getBoundingClientRect();
+    let coordXY = textNode.parentElement.getBoundingClientRect();
+    document.getElementById("coordinates").innerHTML = `Top: ${
+      coordXY.top
+    }, Left: ${coordXY.left}, Bottom: ${coordXY.bottom}, Right: ${
+      coordXY.right
+    }`; // coordinates is id in HTML doc
+    document.getElementById(
+      "coordinates-container"
+    ).innerHTML = `Top: ${coordXY.top -
+      coordXYContainer.top}, Left: ${coordXY.left -
+      coordXYContainer.left}, Bottom: ${coordXYContainer.bottom -
+      coordXY.bottom}, Right: ${coordXYContainer.right - coordXY.right}`;
+    let spaceObj = {
+      top: coordXY.top - coordXYContainer.top,
+      left: coordXY.left - coordXYContainer.left,
+      bottom: coordXYContainer.bottom - coordXY.bottom,
+      right: coordXYContainer.right - coordXY.right
+    };
+    const maxSpace = Math.max(...Object.values(spaceObj));
+    let maxProp = Object.entries(spaceObj).filter(e => e[1] === maxSpace)[0];
+    // console.log(maxSpace)
+    //TODO: width and height of textNode (coordXY) to be taken into consideration
 
-  let popup = textNode.nextElementSibling;
-  popup.classList.add(`show-on-${maxProp[0]}`);
-  // if (maxProp[0] == "left") {
-  //   popup.setAttribute("style", "color: green;");
-  // }
-}
+    let popup = textNode.nextElementSibling;
+    popup.classList.add(`show-on-${maxProp[0]}`);
+    // if (maxProp[0] == "left") {
+    //   popup.setAttribute("style", "color: green;");
+    // }
+  },
+  false
+);
